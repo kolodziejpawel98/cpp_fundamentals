1ed8e5740114331513265934610244ce3f073405
Pawel Kolodziej <kolodziejpawel98@gmail.com>
2025-11-03 23:09:06 +0100

    [w02d03] variadic templates ongoing

--- cpp_skills_training/week_2/day_3/src/OwnMakeUnique.hpp
+++ cpp_skills_training/week_2/day_3/src/OwnMakeUnique.hpp
@@ -12,44 +12,16 @@ template <typename T>
 class MyClass
 {
 public:
+    template <typename... Args>
     MyClass(Args &&...args) : value(std::forward<Args>(args)...) {}
-
-    // dzieki temu to zadziala
-    //     struct Point {
-    //     int x, y;
-    //     Point(int a, int b) : x(a), y(b) {}
-    // };
-
-    // int main() {
-    //     MyClass<Point> p1(1, 2);
-    //     MyClass<Point> p2;
-    // }
-
-    // void print()
-    // {
-    //     std::cout << "a = " << a << std::endl;
-    // }
 
 private:
     T value;
 };
 
-// template <typename T>
-// inline MyClass myOwnMakeUnique(T a)
-// {
-//     MyClass myClassObject{a};
-//     return myClassObject;
-// }
-
-// template <typename... Args>
-// inline void myOwnMakeUnique(const Args &...args)
-// {
-//     (std::cout << ... << args) << '\n';
-// }
-
-template <typename... Args>
-inline void myOwnMakeUnique(Args &&...args)
+template <typename T, typename... Args>
+inline MyClass<T> myOwnMakeUnique(Args &&...args)
 {
-    MyClass myClassObject{std::forward<Args>(args)...};
-    // return myClassObject;
+    MyClass<T> myClassObject{std::forward<Args>(args)...};
+    return myClassObject;
 }
--- cpp_skills_training/week_2/day_3/test/week_2_day_3_tests.cpp
+++ cpp_skills_training/week_2/day_3/test/week_2_day_3_tests.cpp
@@ -1,11 +1,29 @@
 #include <gtest/gtest.h>
 #include "OwnMakeUnique.hpp"
 
+struct Point
+{
+    int x, y;
+    Point(int a, int b) : x(a), y(b) {}
+
+    Point(const Point &point) : x(point.x), y(point.y) { std::cout << "copy constructor!\n"; }
+    Point(const Point &&point) : x(std::move(point.x)), y(std::move(point.y)) { std::cout << "move constructor!\n"; }
+};
+
 TEST(week_2_day_3, basic_scenario)
 {
-    // MyClass myClassObject_1 = myOwnMakeUnique(55);
-    // myClassObject_1.print();
+    [[maybe_unused]] MyClass<Point> p1(1, 2);
 
-    MyClass myClassObject_2 = myOwnMakeUnique(11, 12);
-    // myClassObject_2.print();
+    [[maybe_unused]] MyClass<Point> p2 = myOwnMakeUnique<Point>(1, 2);
+}
+
+TEST(week_2_day_3, passing_lvalue_as_argument)
+{
+    Point p1{1, 2};
+    [[maybe_unused]] MyClass<Point> p2 = myOwnMakeUnique<Point>(p1);
+}
+
+TEST(week_2_day_3, passing_rvalue_as_argument)
+{
+    [[maybe_unused]] MyClass<Point> p2 = myOwnMakeUnique<Point>(Point{1, 2});
 }
